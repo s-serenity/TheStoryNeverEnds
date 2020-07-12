82297f521db166426d355990aa228a7f3eeedc36
s-serenity <476006557@qq.com>
2020-07-12 20:44:03 +0800

    add basic files

---
+++ src/main/java/com/lsown/storychains/TheStoryNeverEndsApplication.java
@@ -0,0 +1,13 @@
+package com.lsown.storychains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheStoryNeverEndsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheStoryNeverEndsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lsown/storychains/TheStoryNeverEndsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsown.storychains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheStoryNeverEndsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
